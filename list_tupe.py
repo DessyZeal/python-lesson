490acd4dd64a43915904ade190fe81d16520e56f
Ude Desire <udesire20@gmail.com>
2025-08-18 21:52:57 +0100

    new commit

--- list_tupe.py
+++ list_tupe.py
@@ -1,2 +1,12 @@
 numbers = [1, 2, 3]
 print(numbers)
+# acessing python list elements
+languages = ["Python", "Java", "C++"]
+print(languages[1])
+# slicing of a python list
+my_list = ['p','o','g','r','a','m','i']
+print(my_list[2:5])
+# printing the last 3 elements of the list
+print(my_list[-3:])
+# printing elements from index 5 to the end
+print(my_list[5:])
