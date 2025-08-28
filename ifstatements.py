0a8f742ef0957a6f38766d430ba3eb62e5d198c3
Ude Desire <udesire20@gmail.com>
2025-08-28 15:34:28 +0100

    new commit

--- ifstatements.py
+++ ifstatements.py
@@ -1,1 +1,9 @@
 # writing an if statement thhat when a person is above 18 show adult
+# first create a variable age
+age = 32
+print("Your age is:", age)
+# if statements
+if age >= 18:
+    print("You are an adult")
+else:
+    print("You are a minor")
