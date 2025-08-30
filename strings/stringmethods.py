a59c4a17e193cbb5e9a2cb102a785635b4467e10
Ude Desire <udesire20@gmail.com>
2025-08-30 20:56:32 +0100

    new commit

---
+++ strings/stringmethods.py
@@ -0,0 +1,21 @@
+# python strings/string methods
+nameLanguage = "Python"
+print(nameLanguage.upper())
+print(nameLanguage.lower())
+# acessing indexes
+print(nameLanguage[3])
+# python strings are immutable
+# multiline strings
+message = """
+my name is Ude Desire
+i love to eat """
+print(message)
+# python string formatting (f strings)
+first_name = "Ude"
+last_name = "Desire"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+
+name1 = "Dessy"
+country = "Nigeria"
+print(f"{name1} is from {country}")
