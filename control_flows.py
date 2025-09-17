61d93289a52e85549cff59b34d46bfa347508675
Ude Desire <udesire20@gmail.com>
2025-09-17 15:23:25 +0100

    added a list of fruits and looped through it

--- control_flows.py
+++ control_flows.py
@@ -1,4 +1,31 @@
 temperature = 30
-
 if temperature > 25:
     print("Hot day")
+    print("Stay hydrated!")
+elif temperature > 20:
+    print("Warm day")
+    print("Enjoy the weather!")
+else: 
+    print("Cold day")
+isStudent = True
+if isStudent == True:
+    print("You are a student")
+else:
+    print("You are not a student")
+
+# College grading System
+grade = 95
+if grade >= 90:
+    print("Grade: A")
+elif grade >= 80:
+    print("Grade: B")
+elif grade >= 70:
+    print("Grade: C")
+elif grade >= 60:
+    print("Grade: D")
+else:
+    print("Grade: F")
+# Example of a for loop
+Fruits = ["Apple", "Banana", "Cherry"]
+for fruit in Fruits:
+    print(f"I love {fruit} !")
